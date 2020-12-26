483ac608c1816333ab491f72445047f155f95e39
tax0r <63975482+tax0r@users.noreply.github.com>
2020-12-26 18:32:13 +0100

    Projektdateien hinzufügen.

---
+++ Tax0r MultiURL parser/Classes/UrlParserClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Tax0r_MultiURL_parser
+{
+    class UrlParserClass
+    {
+
+        //Nimmt sich den Quelltext des URL's und gibt ihn wider.
+        public async Task<string> GetUrlContent(string url)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(2);
+            string content = await httpClient.GetStringAsync(url);
+            return content;
+        }
+
+        //Sucht in dem URL nach anderen URL's und gibt eine ArrayListe mit diesen wider.
+        public string[] SearchForUrls(string content)
+        {
+            Regex regex = new Regex(@"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?", RegexOptions.Multiline);
+            MatchCollection matches = regex.Matches(content);
+            List<string> resultsList = new List<string>();
+            
+            foreach (Match match in matches)
+            {
+                resultsList.Add(match.ToString());
+            }
+
+            string[] results = resultsList.ToArray();
+            return results;
+        }
+
+    }
+}
---
+++ Tax0r MultiURL parser/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tax0r_MultiURL_parser.Classes;
+using System.Drawing;
+using Console = Colorful.Console;
+using System;
+using System.Threading;
+
+namespace Tax0r_MultiURL_parser
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            UrlParserClass parserClass = new UrlParserClass();
+            FileHelperClass helperClass = new FileHelperClass();
+
+            Console.WriteAscii("TAX0R, 2020", Color.LightPink);
+
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+
+            Console.Clear();
+            Console.WriteLine("[Important!]: Please drag&drop you'r URL-list into the Application.", Color.Pink);
+
+            string toReplace = '"'.ToString();
+            string input = Console.ReadLine().Replace(toReplace, string.Empty);
+            Console.WriteLine(input);
+            Console.Clear();
+
+            string[] urls = helperClass.readUrlsFromFile(input);
+            List<string> scrapedUrls = new List<string>();
+            
+            foreach (string url in urls)
+            {
+                if(!url.EndsWith(".png") && !url.EndsWith(".jpg") && !url.EndsWith(".pdf") && !url.EndsWith(".css") && !url.EndsWith(".svg") && !url.EndsWith(".ttf"))
+                {
+                    try
+                    {
+                        Console.WriteLine("[OLD URL]: " + url, Color.Green);
+
+                        string content = await parserClass.GetUrlContent(url);
+
+                        string[] foundUrls = parserClass.SearchForUrls(content);
+
+                        foreach (string foundUrl in foundUrls)
+                        {
+                            Console.WriteLine("[NEW URL]: " + foundUrl, Color.LightGreen);
+                            scrapedUrls.Add(foundUrl);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("[BAD URL]: " + url, Color.Red);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("[BAD URL]: " + url, Color.Red);
+                }
+            }
+
+            List<string> distinctUrls = scrapedUrls.Distinct().ToList();
+
+            Console.Clear();
+            Console.WriteLine("[Success!]: URL's we're scraped successfully!");
+            Console.WriteLine("[Information]: New URL's found: " + scrapedUrls.Count(), Color.LightBlue);
+            Console.WriteLine("[Information]: Distinct URL's found: " + distinctUrls.Count(), Color.LightPink);
+
+            helperClass.saveToFile(distinctUrls.ToArray(), distinctUrls.Count());
+        }
+    }
+}
